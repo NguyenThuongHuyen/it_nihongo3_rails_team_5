9ea4ddcabd200f814f213817b16d26254a4a6add
NguyenThuongHuyen <thuonghuyen297@gmail.com>
2021-12-14 02:29:23 +0000

    Add validate image

--- app/models/youtube_channel.rb
+++ app/models/youtube_channel.rb
@@ -1,6 +1,14 @@
 class YoutubeChannel < ApplicationRecord
     has_one_attached :image
     has_many :youtube_channel_review, -> { order "created_at DESC"}
+validate :correct_image_type
 
+def correct_image_type
+        if (image.attached? && !image.content_type.in?(%w(image/jpeg image/png)))
+            errors.add(:image, 'must be a JPEG or PNG.')
+        elsif image.attached? ==false
+            errors.add(:image, 'must be attached.')
+        end
+    end
 
 end
